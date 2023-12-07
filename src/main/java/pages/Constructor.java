558b6e30876f7f45946bd498cd5caffdce1594d3
klirovskij <135542945+klirovskij@users.noreply.github.com>
2023-12-07 09:37:47 +0300

    Update Constructor.java

--- src/main/java/pages/Constructor.java
+++ src/main/java/pages/Constructor.java
@@ -1,5 +1,6 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,13 +10,10 @@ import java.time.Duration;
 
 public class Constructor {
     private final WebDriver driver;
-    //Заголовок h1 главной страницы
     private final By mainH1 = By.xpath("//h1[text()='Соберите бургер']");
-    //локаторы кнопок меню
     private final By linkBuns = By.xpath("//span[text()='Булки']");
     private final By linkSauces = By.xpath("//span[text()='Соусы']");
     private final By linkToppings = By.xpath("//span[text()='Начинки']");
-    //локаторы заголовков разделов в меню
     private final By spanBuns = By.xpath("//div[@class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']/span[text()='Булки']");
     private final By spanSauces = By.xpath("//div[@class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']/span[text()='Соусы']");
     private final By spanToppings = By.xpath("//div[@class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']/span[text()='Начинки']");
@@ -24,37 +22,39 @@ public class Constructor {
         this.driver = driver;
     }
 
+    @Step("Ожидание загрузки главной страницы")
     public void waitForLoadMainPage() {
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(mainH1));
     }
 
-    //метод клика по кнопке "Булки"
+    @Step("Клик по кнопке 'Булки'")
     public void clickBunsLink() {
         driver.findElement(linkBuns).click();
     }
 
-    //метод клика по кнопке "Соусы"
+    @Step("Клик по кнопке 'Соусы'")
     public void clickSaucesLink() {
         driver.findElement(linkSauces).click();
     }
 
-    //метод клика по кнопке "Начинки"
+    @Step("Клик по кнопке 'Начинки'")
     public void clickToppingsLink() {
         driver.findElement(linkToppings).click();
     }
 
+    @Step("Проверка отображения раздела 'Булки'")
     public boolean bunsIsDisplayed() {
         return driver.findElement(spanBuns).isDisplayed();
     }
 
+    @Step("Проверка отображения раздела 'Соусы'")
     public boolean saucesIsDisplayed() {
         return driver.findElement(spanSauces).isDisplayed();
     }
 
+    @Step("Проверка отображения раздела 'Начинки'")
     public boolean toppingsIsDisplayed() {
         return driver.findElement(spanToppings).isDisplayed();
     }
-
-
 }
